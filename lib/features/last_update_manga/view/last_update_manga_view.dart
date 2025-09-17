1bbbcacdca79e5a4ee5c1a10a7e2d6e0274e3f8f
DESKTOP-BGRGULH\DELL <karem8944@gmail.com>
2025-09-17 15:14:44 +0300

    adding last updating manga feature

---
+++ lib/features/last_update_manga/view/last_update_manga_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:otaku_scope/core/providers/providers.dart';
+import 'package:otaku_scope/core/widgets/custom_grid_view.dart';
+
+class LastUpdateMangaView extends ConsumerWidget {
+  const LastUpdateMangaView({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return CustomGridView(provider: lastUpdateMangaNotifierProvider);
+  }
+}
